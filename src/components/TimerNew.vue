8085a184599c7f5b6efffbee2f378a510325686a
Damien Dart <damiendart@pobox.com>
2024-08-24 21:38:34 +0100

    Update copyright-related bumf.

--- src/components/TimerNew.vue
+++ src/components/TimerNew.vue
@@ -1,4 +1,8 @@
 <script setup>
+  // Copyright (C) Damien Dart, <damiendart@pobox.com>.
+  // This file is distributed under the MIT licence. For more
+  // information,please refer to the accompanying "LICENCE" file.
+
   import { ref } from 'vue';
   import useTimerStore from '../stores/timers';
 
--- src/stores/timers.js
+++ src/stores/timers.js
@@ -1,3 +1,7 @@
+// Copyright (C) Damien Dart, <damiendart@pobox.com>.
+// This file is distributed under the MIT licence. For more information,
+// please refer to the accompanying "LICENCE" file.
+
 import { defineStore } from 'pinia';
 import { computed, ref } from 'vue';
 import Timer from '../entities/Timer';
